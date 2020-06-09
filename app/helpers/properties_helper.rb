b4b735ce801e3c065ea954fd6bf876e8f5cea7e6
smcepeda <smcepeda@gmail.com>
2020-06-09 12:08:45 +0200

    Added thumbnail to pictures

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -2,6 +2,6 @@ module PropertiesHelper
 
   def property_thumbnail property
     img = property.photo.present? ? property.photo.thumb.url : 'placeholder.png'
-    image_tag img, class: 'porperty-thumb'
+    image_tag img, class: "property-thumb"
   end
 end
